313e4998b241213d5c7be1ad0860e0cac834797f
dawid.marszalik <dawid.marszalik.sdm@gmail.com>
2023-09-22 18:50:48 +0200

    Fixed overfitting problem for offers that was multiplied as they were same remote job offer but from different cities

--- predict/offer.py
+++ predict/offer.py
@@ -11,7 +11,7 @@ class Offer:
 
         with open('output.txt', 'r') as file:
             lines = file.readlines()
-            self.top_20_skills = [line.strip() for line in lines]
+            self.top_30_skills = [line.strip() for line in lines]
 
         offer_data = []
 
@@ -22,6 +22,8 @@ class Offer:
             offer_data.append(new_row)
 
         self.offer_df = pd.DataFrame(offer_data)
+
+    encoder = joblib.load('one_hot_encoder1.2.2.pkl')
 
     def standardize_text(self, df, text_field):
         df[text_field] = df[text_field].str.lower()
@@ -43,26 +45,20 @@ class Offer:
         self.offer_df = self.standardize_text(self.offer_df, "title")
         self.offer_df = self.standardize_text(self.offer_df, "skill_name")
 
-        offer_dummies = pd.DataFrame()
+        offer_skills_df = self.offer_df.copy()
+        mask = ~offer_skills_df['skill_name'].isin(self.top_30_skills)
+        offer_skills_df.loc[mask, 'skill_name'] = None
 
-        for col_name in sorted(self.top_20_skills):
-            offer_dummies["skill_" + col_name] = 0
+        offer_skills_df = offer_skills_df.dropna(subset=['skill_name'])
 
-        offer_dummies.loc[0] = 0
+        dummies_offer = self.encoder.transform(offer_skills_df[['skill_name']])
 
-        final = []
-        for skill in self.offer_df['skill_name']:
-            new_dummy = offer_dummies.copy()
-            if skill in self.top_20_skills:
-                new_dummy[f"skill_{skill}"] = 1
-            final.append(new_dummy)
+        multiplier_offer = offer_skills_df['skill_level'].values
+        dummies_offer_df = pd.DataFrame.sparse.from_spmatrix(dummies_offer, columns=self.encoder.get_feature_names_out(['skill_name']))
 
-        all_dummies = pd.concat(final, ignore_index=True)
+        dummies_offer_df = dummies_offer_df.mul(multiplier_offer, axis=0)
 
-        for col in all_dummies.columns:
-            all_dummies[col] *= self.offer_df['skill_level']
-
-        aggregated_offer_df = pd.concat([self.offer_df, all_dummies], axis=1)
+        aggregated_offer = pd.concat([offer_skills_df, dummies_offer_df], axis=1)
 
         def custom_agg(series):
             if series.name.startswith('skill'):
@@ -70,28 +66,32 @@ class Offer:
             else:
                 return series.iat[0]
 
-        aggregated_offer_df = aggregated_offer_df.drop('skill_name', axis=1)
-        aggregated_offer_df = aggregated_offer_df.drop('skills', axis=1)
+        aggregated_offer = aggregated_offer.drop(['skills', 'skill_name', 'skill_level'], axis=1)
 
-        aggregated_offer_df = aggregated_offer_df.groupby('id', as_index=False).agg(custom_agg)
-        aggregated_offer_df = aggregated_offer_df.drop('skill_level', axis=1)
+        aggregated_offer_df = aggregated_offer.groupby('id', as_index=False).agg(custom_agg)
 
         level_mapping = {'junior': 0, 'mid': 1, 'senior': 2}
-        workplace_mapping = {'remote': 0, 'partly_remote': 1, 'office': 2}
-        country_mapping = {'PL': 0, 'SK': 1, 'CZ': 2, 'HU': 3, 'AT': 4, 'SI': 5, 'RO': 6, 'MT': 7, 'DE': 8, 'UA': 9, 'None': 10, 'EE': 11}
+        workplace_mapping= {'remote': 0, 'partly_remote': 1, 'office': 2}
+        country_mapping = {'PL': 0, 'SK': 1, 'CZ': 2, 'HU': 3, 'AT': 4, 'SI': 5, 'RO': 6, 'MT': 7, 'DE': 8, 'UA': 9, 'None': 10,
+            'EE': 11}
 
         aggregated_offer_df['experience_level'] = aggregated_offer_df['experience_level'].map(level_mapping)
-        aggregated_offer_df['workplace_type'] = aggregated_offer_df['workplace_type'].map(workplace_mapping)
+
+        aggregated_offer_df['workplace_type'] =aggregated_offer_df['workplace_type'].map(workplace_mapping)
+
         aggregated_offer_df['country_code'] = aggregated_offer_df['country_code'].map(country_mapping)
+        aggregated_offer_df['country_code'] = aggregated_offer_df['country_code'].fillna(10)
 
         offer_employment_types = aggregated_offer_df['employment_types'].apply(pd.Series)
         offer_employment_type = offer_employment_types[0].apply(pd.Series)
-        offer_employment_type = pd.concat([offer_employment_type.drop(['salary'], axis=1), offer_employment_type['salary'].apply(pd.Series)], axis=1)
+        offer_employment_type= pd.concat([offer_employment_type.drop(['salary'], axis=1),
+                                        offer_employment_type['salary'].apply(pd.Series)], axis=1)
 
-        offer_no_employment = aggregated_offer_df.drop(columns='employment_types')
-        offer_employment = pd.concat([offer_no_employment, offer_employment_type], axis=1)
+        offer_no_employment = aggregated_offer_df.drop(columns = 'employment_types')
+        offer_employment = pd.concat([offer_no_employment, offer_employment_type], axis = 1)
 
         type_map = {'permanent': 0, 'b2b': 1, 'mandate_contract': 2}
+        self.reversed_type_map = {v: k for k, v in type_map.items()}
         offer_employment['type'] = offer_employment['type'].map(type_map)
 
         def standardize_company_size(company_size_value):
@@ -112,21 +112,8 @@ class Offer:
 
         offer_employment['avg_company_size'] = offer_employment['company_size'].apply(lambda x: standardize_company_size(x))
         
-        scaler = joblib.load('Standard_Scaler_1.2.2.pkl')
-        self.offer = offer_employment.drop(columns=['id', 'from', 'to', 'currency', 'company_size', 'company_profile', 'apply_body', 'title', 'street', 'city', 'address_text', 'marker_icon', 'company_name', 'company_url', 'latitude', 'longitude', 'apply_url', 'published_at', 'remote_interview', 'video_key', 'video_provider', 'open_to_hire_ukrainians', 'future_consent_title', 'future_consent', 'information_clause', 'custom_consent_title', 'custom_consent', 'tags', 'body', 'company_logo_url', 'banner_url', 'multilocation'] , errors='ignore')
-        self.offer_scaled = pd.DataFrame(scaler.transform(self.offer), columns = self.offer.columns)
-        
-
+        offer = offer_employment.drop(columns = ['id', 'from', 'to', 'apply_body', 'title', 'company_size', 'street', 'city', 'address_text', 'marker_icon', 'company_name', 'company_url', 'latitude', 'longitude', 'apply_url', 'published_at', 'remote_interview', 'video_key', 'video_provider', 'open_to_hire_ukrainians', 'future_consent_title', 'future_consent', 'information_clause', 'custom_consent_title', 'custom_consent', 'tags', 'body', 'company_logo_url', 'banner_url', 'multilocation', 'company_profile', 'currency'], errors='ignore')
+        self.offer = offer.reindex(sorted(offer.columns), axis=1)
 
     def get_processed_offer(self):
-        return self.offer_scaled
-
-
-# job_offer_url = "https://justjoin.it/offers/winged-it-lead-data-scientist"
-# offer_instance = Offer(job_offer_url)
-# offer_instance.preprocess()
-# processed_offer = offer_instance.get_processed_offer()
-# print('Wiersz bez standaryzacji: \n' ,processed_offer)
-# scl = StandardScaler()
-# offer_scaled = pd.DataFrame(scl.fit_transform(processed_offer), columns = processed_offer.columns)
-# print('wiersz po standaryzacji: \n', offer_scaled)
+        return self.offer
--- predict/views.py
+++ predict/views.py
@@ -7,13 +7,13 @@ from predict.offer import Offer
 model_from = joblib.load('best_model_from_1.2.2.pkl')
 model_to = joblib.load('best_model_to_1.2.2.pkl')
 
-# job_offer_url = "https://justjoin.it/offers/winged-it-lead-data-scientist"
+# job_offer_url = "https://justjoin.it/offers/crimson-panda-sql-data-engineer"
 # offer_instance = Offer(job_offer_url)
 # offer_instance.preprocess()
 # processed_offer = offer_instance.get_processed_offer()
 # print(processed_offer)
 
-# pred = model.predict(processed_offer)
+# pred = model_from.predict(processed_offer)
 
 # print(pred)
 
@@ -27,11 +27,11 @@ def prediction(request):
         offer_instance = Offer(job_offer_url)
         offer_instance.preprocess()
         processed_offer = offer_instance.get_processed_offer()
-    salary_from = round(model_from.predict(processed_offer)[0], -2)
-    salary_to = round(model_to.predict(processed_offer)[0], -2)
-
+    salary_from = round(model_from.predict(processed_offer)[0]/100)*100
+    salary_to = round(model_to.predict(processed_offer)[0]/100)*100
     context = {'salary_from': salary_from,
-               'salary_to': salary_to
+               'salary_to': salary_to,
                }
+    
     print(context)
     return render(request, "predict.html", context)
